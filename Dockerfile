975c27d1393a0a67f2b5c581bfa49e0362884eee
Nick <n.krijgsman@student.fontys.nl>
2021-06-12 18:22:51 +0200

    Added feed and fixes

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM maven:3.8.1-jdk-11 as maven
+WORKDIR /app
+COPY ./pom.xml ./pom.xml
+RUN mvn dependency:go-offline -B
+COPY ./src ./src
+
+RUN mvn package && cp target/gateway-init.jar app.jar
+
+# Rely on Docker's multi-stage build to get a smaller image based on JRE
+FROM openjdk:11-jre
+WORKDIR /app
+COPY --from=maven /app/app.jar ./app.jar
+
+EXPOSE 8085
+
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/kwetter/gateway/GatewayApplication.java
+++ src/main/java/com/kwetter/gateway/GatewayApplication.java
@@ -2,15 +2,18 @@ package com.kwetter.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableWebFlux
+//@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 
 }
