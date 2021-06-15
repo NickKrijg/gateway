525fc373a64bc5d2aafc42e6c61ed48768b2d95c
Nick <n.krijgsman@student.fontys.nl>
2021-06-15 23:35:05 +0200

    Added CORS and routes

--- src/main/java/com/kwetter/gateway/GatewayApplication.java
+++ src/main/java/com/kwetter/gateway/GatewayApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.reactive.config.EnableWebFlux;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableWebFlux
-//@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
